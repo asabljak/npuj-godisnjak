adb4253b6dfea9ab7e36db9b24d1af98543e2ece
asabljak <asabljak@nbt00791.int.sedamIT.hr>
2017-06-24 14:08:03 +0200

    View resolver setup

--- src/main/java/hr/tvz/java/godisnjak/web/ApplicationUserTypeController.java
+++ src/main/java/hr/tvz/java/godisnjak/web/ApplicationUserTypeController.java
@@ -54,7 +54,7 @@ public class ApplicationUserTypeController {
     @RequestMapping(produces = "text/html")
     public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
     	
-        uiModel.addAttribute(repository.findAllByOrderByNameAsc());
+        uiModel.addAttribute("applicationusertypes", repository.findAllByOrderByNameAsc());
 
         return "applicationusertypes/list";
     }
